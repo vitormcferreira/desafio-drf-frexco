4ddcd07bf55b096fe7005d1281d60781fd4d56fb
Vitor <>
2022-01-16 09:12:21 -0300

    documentar RegionViewSet e FruitViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from . import views
 
@@ -10,4 +11,9 @@ router.register('fruit', views.FruitViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    # Documentação
+    path('schema/', SpectacularAPIView().as_view(), name='schema'),
+    # link para a visualização da API
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,31 @@
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from .models import Region, Fruit
 from .serializers import RegionSerializer, FruitSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(summary='Obtém todas as regiões'),
+    create=extend_schema(summary='Insere uma nova região.'),
+    retrieve=extend_schema(summary='Obtém uma região.'),
+    update=extend_schema(summary='Atualiza uma região.'),
+    partial_update=extend_schema(summary='Atualiza parcialmente uma região.'),
+    destroy=extend_schema(summary='Remove uma região.'),
+)  # Documentação
 class RegionViewSet(viewsets.ModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
 
 
+@extend_schema_view(
+    list=extend_schema(summary='Obtém todas as frutas.'),
+    create=extend_schema(summary='Insere uma nova fruta.'),
+    retrieve=extend_schema(summary='Obtém uma fruta.'),
+    update=extend_schema(summary='Atualiza uma fruta.'),
+    partial_update=extend_schema(summary='Atualiza parcialmente uma fruta.'),
+    destroy=extend_schema(summary='Remove uma fruta.'),
+)  # Documentação
 class FruitViewSet(viewsets.ModelViewSet):
     queryset = Fruit.objects.all()
     serializer_class = FruitSerializer
