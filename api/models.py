06de8552fbff791362a2b126e864f1eef12ca6f6
Vitor <>
2022-01-16 08:52:17 -0300

    adicionar models Region e Fruit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    class Meta:
+        db_table = 'Region'
+        ordering = ['name']
+
+    class RegionsChoices(models.TextChoices):
+        NORTE = 'Norte'
+        NORDESTE = 'Nordeste'
+        CENTRO_OESTE = 'Centro-Oeste'
+        SUDESTE = 'Sudeste'
+        SUL = 'Sul'
+
+    name = models.CharField(
+        max_length=12,
+        choices=RegionsChoices.choices,
+        unique=True,
+    )
+
+
+class Fruit(models.Model):
+    class Meta:
+        db_table = 'Fruit'
+        ordering = ['name']
+
+    name = models.CharField(max_length=255)
+    region_id = models.ForeignKey(
+        to=Region, on_delete=models.SET_NULL, null=True)
