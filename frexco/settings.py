950d0651c2a5529ba41c2dddc45fc9c608fc90f0
Vitor <>
2022-02-25 08:23:35 -0300

    remove classes de autenticação

--- frexco/settings.py
+++ frexco/settings.py
@@ -132,6 +132,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    # remove classses de autenticação
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 # https://drf-spectacular.readthedocs.io/en/latest/settings.html
